53a9eabb780b54d32df1b518f7e4d713f61e721f
Robin Millette <robin@millette.info>
2019-04-06 11:37:07 -0400

    Take URL as first argument.

--- d.js
+++ d.js
@@ -1,16 +1,32 @@
+#!/usr/bin/env node
+
 'use strict'
 
+// core
+const { URL } = require('url')
+
+// npm
 const puppeteer = require('puppeteer')
 
 const run = async () => {
+  const u = process.argv[2] && new URL(process.argv[2])
+  if (!u) throw new Error('URL required.')
   const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('https://millette.github.io/kyucan/')
-  const cnt = await page.content()
-  await browser.close()
-  return cnt
+  let cnt
+  try {
+    const page = await browser.newPage()
+    await page.goto(u)
+    cnt = await page.content()
+  } finally {
+    await browser.close()
+    if (cnt) return cnt
+    throw new Error('Oups')
+  }
 }
 
 run()
   .then(console.log)
-  .catch(console.error)
+  .catch((err) => {
+    console.error(err)
+    process.exitCode = 1
+  })
